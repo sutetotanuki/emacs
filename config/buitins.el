833f7ffd5174ee29659ee602fec2148d3ff629ac
sutetotanuki <sutetotanuki@gmail.com>
2012-09-12 09:49:26 +0900

    Start using uniqify

--- config/buitins.el
+++ config/buitins.el
@@ -1,4 +1,13 @@
 ;;; ビルドインのモジュールに対する設定
 
-;; cc-mode
+;;; cc-mode
 (load "config/builtins/cc-mode")
+
+
+;;; uniquify (バッファ名が被った時にわかりやすくする)
+(require 'uniquify)
+;; finename<dir>の形式にする
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+;; *で囲まれたバッファ名は対象外にする
+(setq uniquify-ignore-buffers-rs "*[^*]+*")
